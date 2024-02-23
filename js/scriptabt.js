f2766c0a3d74559071abf7c2b049d41bd719d593
KING_CODES <157330782+kingcodes001@users.noreply.github.com>
2024-02-23 18:32:23 +0000

    Update scriptabt.js

--- js/scriptabt.js
+++ js/scriptabt.js
@@ -45,30 +45,30 @@ window.onscroll = ()=> {
 
 
 
-ScrollReveal({
-     reset: true,
-     distance : '60px',
-     duration: 25000,
-     delay: 400
-    });
+// ScrollReveal({
+//      reset: true,
+//      distance : '60px',
+//      duration: 25000,
+//      delay: 400
+//     });
 
 
 
-    //scrollrevial in about us section
-    ScrollReveal().reveal('.king-about-head , .king-contact-intro h1 , .king-contact-intro p , .service-head ,  .king-projects h1 ', { delay: 200 , origin: 'left' });
-    ScrollReveal().reveal('.king-about-content', { delay: 300 , origin: 'right' });
-    ScrollReveal().reveal('.king-image img', { delay: 400 , origin: 'left' });
-    ScrollReveal().reveal('.readMore-btn', { delay: 800 , origin: 'bottom' });
+//     //scrollrevial in about us section
+//     ScrollReveal().reveal('.king-about-head , .king-contact-intro h1 , .king-contact-intro p , .service-head ,  .king-projects h1 ', { delay: 200 , origin: 'left' });
+//     ScrollReveal().reveal('.king-about-content', { delay: 300 , origin: 'right' });
+//     ScrollReveal().reveal('.king-image img', { delay: 400 , origin: 'left' });
+//     ScrollReveal().reveal('.readMore-btn', { delay: 800 , origin: 'bottom' });
     
-    // //scrollrevial in project section
-    ScrollReveal().reveal('.king-projects-container', { delay: 550 , origin: 'top', interval: 100 });
+//     // //scrollrevial in project section
+//     ScrollReveal().reveal('.king-projects-container', { delay: 550 , origin: 'top', interval: 100 });
     
-    //scrollrevial in service section
-    ScrollReveal().reveal('.service-box', { delay: 550 , origin: 'bottom', interval: 100 });
+//     //scrollrevial in service section
+//     ScrollReveal().reveal('.service-box', { delay: 550 , origin: 'bottom', interval: 100 });
     
-    // //scrollrevial in contact me section
-    ScrollReveal().reveal('.king-form', { delay: 500 , origin: 'right' });
-    ScrollReveal().reveal('.king-contact-info', { delay: 560 , origin: 'top', interval: 200 });
+//     // //scrollrevial in contact me section
+//     ScrollReveal().reveal('.king-form', { delay: 500 , origin: 'right' });
+//     ScrollReveal().reveal('.king-contact-info', { delay: 560 , origin: 'top', interval: 200 });
 
     
 
